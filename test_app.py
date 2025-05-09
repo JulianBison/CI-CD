757486fbbd068e14c2fb7aadf606c184dfa5bb9d
Julian Bison <80854425+JulianBison@users.noreply.github.com>
2025-05-09 20:02:43 -0300

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -4,6 +4,12 @@ def suma(a, b):
 def resta(a, b):
     return a - b
 
+def multiplication(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+    
 
 def test_suma():
     assert suma(2, 3) == 5
@@ -15,7 +21,13 @@ def test_resta():
     assert resta(5, 2) == 3
     assert resta(1, 1) == 1
 
+def test_multiplication():
+    assert multiplication(3,3) == 9
+    assert multiplication(2,5) == 10
 
+def test_divide():
+    assert divide(12,3) == 4
+    assert divide(20,4) == 5
 
 
 
